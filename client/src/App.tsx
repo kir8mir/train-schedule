fbf2fffe4c3ed1cbcfe04887dc489f8fb5387770
Cyrill Prudchenko <49589547+kir8mir@users.noreply.github.com>
2022-11-30 11:26:17 +0200

    Empty React app created

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>You are able to Do whatever you want. Just Do It!</h1>
+    </div>
+  );
+}
+
+export default App;
